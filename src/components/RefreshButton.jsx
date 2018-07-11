4304c9440bf1d4987d59a39ad9972694371d98e7
nishiurahiroki <nishiurahiroki@gmail.com>
2018-07-11 08:16:44 -0400

    refactoring.

--- src/components/RefreshButton.jsx
+++ src/components/RefreshButton.jsx
@@ -1,5 +1,5 @@
 import {h} from 'hyperapp'
 
-export default (props, children) => (state, action) => (
-  <i class="fa fa-refresh" onclick={props.onclick} style={props.style}></i>
+export default ({onclick, style}, children) => () => (
+  <i class="fa fa-refresh" onclick={onclick} style={style}></i>
 )
